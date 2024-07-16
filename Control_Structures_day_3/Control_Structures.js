c303b0b618ed60c941d9f84fa6f992f4c110a8bc
M13chand <mannuc359@gmail.com>
2024-07-16 09:59:12 +0545

    control structures day 3 completed

--- Control_Structures_day_3/Control_Structures.js
+++ Control_Structures_day_3/Control_Structures.js
@@ -113,4 +113,129 @@ switch (true) {
 
 // Activity 4: Conditional (Ternary ) Operator
 
-// Task 6:
+// Task 6:Write a program that uses the ternary operator to check if a number is odd or even and log the result to the console.
+
+let a = 30;
+let numberOddEven = (a % 2 == 0) ? "The number is Even" : "The number is odd";
+console.log(numberOddEven);
+
+// Activity 5:Combining Conditions
+
+// Task 7: Write a program to check if a year is  leap year using multiple conditions (divisible by 4 ,but not 100 unless also divisible by 400) and log the result to the console.
+
+let year = 2020;
+
+let leapYear = (year % 4 == 0 && year % 100 !== 0 || year % 400 == 0) ? `The ${year} is leap year` : `The ${year} is not leap year`;
+
+console.log(leapYear);
+
+
+// Feature Request:
+// 1. Number check script:Write a script that checks if a number is positive,negative, or zero using if-else statements and logs the result.
+
+let scriptNumber = "infinity";
+if (scriptNumber > 0) {
+  console.log("The number is positive number ");
+
+} else if (scriptNumber < 0) {
+  console.log("The number is negative number");
+} else if (scriptNumber == 0) {
+  console.log("The number is zero");
+} else {
+  console.log("The given number is not a valid number ");
+}
+
+
+// 2.Voting Eligibility Script:Create a to check if a person is eligible to vote based o their age and log the result.
+
+let votingAge = 17;
+
+if (votingAge >= 18) {
+  console.log("You are eligible to vote.");
+} else if (votingAge < 18) {
+  console.log("You are not eligible to vote.");
+} else {
+  console.log("The provided number is not a valid age.");
+}
+
+// 3.Day of the week Script:Write a script that uses a switch case to determine the day of the week based on a number (1-7) and logs the day name.
+let dayOfWeek = 4;
+switch (dayOfWeek) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("The day of the week is not valid");
+
+}
+
+// 4. Grade Assignment Script: Create a script that uses a switch case to assign grade based on a score and logs the grade.
+
+let mark = 40;
+switch (true) {
+  case 90 <= mark && 100 >= mark:
+    console.log("Your grade is A+");
+    break;
+
+  case 80 <= mark && 89 >= mark:
+    console.log("Your grade is A");
+    break;
+  case 70 <= mark && 79 >= mark:
+    console.log("Your grade is B+");
+    break;
+
+  case 60 <= mark && 69 >= mark:
+    console.log("Your grade is B");
+    break;
+
+  case 50 <= mark && 59 >= mark:
+    console.log("Your grade is C+");
+    break;
+
+  case 40 <= mark && 49 >= mark:
+    console.log("Your grade is C");
+    break;
+
+  case 30 <= mark && 39 >= mark:
+    console.log("Your grade is D+");
+    break;
+
+  case 20 <= mark && 29 >= mark:
+    console.log("Your grade is D");
+    break;
+
+  case 0 <= mark && 19 >= mark:
+    console.log("Your grade is E");
+
+  default:
+    console.log("Invalid Mark");
+}
+
+
+//5. Leap Year Check Script: Write a script that checks if a year is a leap year using multiple conditions and logs the result.
+
+let yearToCheck = 2020;
+if ((yearToCheck % 4 == 0) && (yearToCheck % 100 != 0) || (yearToCheck % 400 == 0)) {
+  console.log("This year is a leap year");
+} else {
+  console.log("This year is not a leap year");
+}
+
